ded762362f39b3cabce6df7073781732f79dcd84
plateauh <70834431+plateauh@users.noreply.github.com>
2021-09-25 22:13:02 +0300

    working perfectly, needs some plishes

--- app/src/main/java/com/example/buttonapp/MainActivity.kt
+++ app/src/main/java/com/example/buttonapp/MainActivity.kt
@@ -2,10 +2,34 @@ package com.example.buttonapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import kotlin.math.min
+
+lateinit var number : TextView
+lateinit var minus : Button
+lateinit var plus : Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        number = findViewById(R.id.tvNumber)
+        minus = findViewById(R.id.minusBtn)
+        plus = findViewById(R.id.plusBtn)
+
+        minus.setOnClickListener {
+            var currentNum = number.text.toString().toInt()
+            currentNum--
+            number.text = currentNum.toString()
+        }
+
+        plus.setOnClickListener {
+            var currentNum = number.text.toString().toInt()
+            currentNum++
+            number.text = currentNum.toString()
+        }
+
     }
 }
